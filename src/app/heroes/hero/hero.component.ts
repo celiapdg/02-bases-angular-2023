a7ab3c7b9fa3efec4df708cd061b736c0a9fe5e1
Celia <celia2795@gmail.com>
2023-05-08 18:57:29 +0200

    heroes

---
+++ src/app/heroes/hero/hero.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes-hero',
+  templateUrl: './hero.component.html',
+  styleUrls: ['./hero.component.css']
+})
+export class HeroComponent {
+
+  public nombre: string = 'ironman';
+  public edad: number = 45;
+
+}
